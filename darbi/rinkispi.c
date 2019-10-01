65d9654fb0aae6b715d84f1772c4fbe417745ddf
root <floby132@gmail.com>
2019-10-01 23:26:18 +0100

    20191002_01_25

--- darbi/rinkispi.c
+++ darbi/rinkispi.c
@@ -1,14 +1,17 @@
+
+#include<math.h>
 #include<stdio.h>
 
-float pi() { return 3.14; } ;
+float PI;
 
-int main () {
-int R=123 ;
-double L=2*pi()*R ;
+void main () {
+	int R=123 ;
+	double L=2*PI*R ;
 
 printf("Rinka ar raadiusu %d garums ir %d metri", R, L);
 
 return 0;
+
 }
 
 
