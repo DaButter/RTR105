3dca21a43c800c853d40624c899563221a411067
user <floby132@gmail.com>
2019-10-31 15:57:40 +0200

    2019_31_10-15_57

---
+++ darbi/double_argumenta_kvadrats.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+ 
+//function declaration 
+
+ float squar(float in);
+
+ //functiondefinition
+
+ int main() {
+	 float x;
+		 for(x=0 ; x<1 ; x+=0.1){ 
+			printf("%f %f\n", x, squar(x));
+		}
+             }
+
+  //function definition 
+     float squar(float in) {
+        	return in * in;
+	}
