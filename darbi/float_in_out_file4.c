89a5d801ea6dd66ff8929c7d7332f397c34bb4d3
root <floby132@gmail.com>
2019-11-09 20:05:58 +0000

    2019_11_09-22_54

--- darbi/float_in_out_file4.c
+++ darbi/float_in_out_file4.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main() {
 
@@ -8,34 +9,36 @@ int main() {
  FILE *fptr;
 
  fptr = fopen("float_teksta_fails.txt","w");
-
  if(fptr == NULL) {
 	printf("Error!");
 	exit(1);
 	} 
+
  for(int i=0; i<5; i++) {
 	for(int j=0; j<2; j++) {
 		printf("Ievadiet %d. rindas un %d. kolonnas mainigo: ",i+1,j+1);
-		scanf("%lf", &mas1[i][j]);
+		scanf("%lf",&mas1[i][j]);
 		}
 	}
  for(int i=0; i<5; i++) {
 	for(int j=0; j<2; j++) {
-		fprintf(fptr,"%f",mas1[i][j]);
+		fprintf(fptr,"%f ",mas1[i][j]);
 		}
-	 fprintf(fptr," \n");
+	 fprintf(fptr,"\n");
 	}
-	fclose(fptr);
 
- if ((fptr = fopen("float_teskta_fails.txt", "r")) == NULL) {
-	printf("Error opening file!");
+ fclose(fptr);
+
+ if ((fptr = fopen("float_teskta_fails.txt","r")) == NULL) {
+	printf("Error when opening file!");
 	exit(1);
 	}
+
  for(int i=0; i<5; i++) {
 	for(int j=0; j<2; j++) {
-		fscanf(fptr, "%lf", &mas2[i][j]);
+		fscanf(fptr, "%lf ",&mas2[i][j]);
 		}
-	    printf("%f %f %f\n", mas2[i][0],mas2[i][1],mas2[i][0]*mas2[i][1]);
+	    printf("%.3f %.3f %.3f\n",mas2[i][0],mas2[i][1],mas2[i][0]*mas2[i][1]); //noapaloju 3 ciparu aiz komata, ertak uztvert
 	}
  fclose(fptr);
  return 0;
