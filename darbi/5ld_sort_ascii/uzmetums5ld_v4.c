10c59223a4e1975b1da8a5a117b1948929e2dd55
root <floby132@gmail.com>
2020-01-06 19:12:16 +0000

    20200106_2343

--- darbi/5ld_sort_ascii/uzmetums5ld_v4.c
+++ darbi/5ld_sort_ascii/uzmetums5ld_v4.c
@@ -1,32 +1,53 @@
 #include <stdio.h>
 
+
 int main()
 {
-	int test[25]={},i=0,j;
+	int text[25]={},i=0,j;
 	char a[25];
+	int max=0,min;
 
 	printf("Ludzu, ievadiet simbolu rindu: ");
 	scanf("%[^\n]s", &a);
 
 	while(a[i]!='\0')
 	{
-		test[i]=a[i];
-		i++;
+		text[i]=a[i]; //char vertibas saglaba int test[], iegust ascii
+		i++; //i=ievadito simbolu skaits
 	}
 
 	printf("Jusu ievaditais teksts: %s \n",a);
-	printf("Jusu ievadita teksta ANSII ekv: ");
+	printf("Jusu ievadita teksta ANSII ekvivalents: ");
 
 	for(j=0;j<i;j++)
 	{
-		printf("%d ",test[j]);
+		printf("%d ",text[j]); //print ascii vertibas
 	}
 	printf("\n");
 
-	//simbols ar min un max value
+	for(j=0;j<i;j++)
+	{
+		min = text[0];
+
+		if(text[j] > max) //atrod max ascii vertibu
+		{
+			max = text[j];
+		}
+
+		if(text[j] <= min) //atrod min ascii vertibu
+		{
+			min = text[j];
+		}
+	}
+
+	printf("Max pec ASCII ir %d \n",max);
+	printf("Min pec ASCII ir %d \n",min);
+
+	//simbols ar min un max value (CHECK)
 	//videjo
 	//mediana
 	//moda vai modas
+	//augosa seciba pec ansii
 	//norada zem uzd vertibas
 
 return 0;
