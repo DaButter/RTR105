f6e071fe6540df31013700cd7fdf018dd402a5c7
root <floby132@gmail.com>
2020-01-19 15:20:45 +0000

    20201901_17-20

--- darbi/4ld_integral/piemers2.c
+++ darbi/4ld_integral/piemers2.c
@@ -1,49 +1,60 @@
-#include<stdio.h>
-#include<math.h>
-float f(float x);
+#include <stdio.h>
+#include <math.h>
+
 void main(){
- int k,n=2,m=1;
- float a=0.,b=0,h,delta_x=0,int1=0.,int2=0.,int3=0.;
 
-  printf("Lietotājs,lūdzu, ievadi sākuma robežu: ");
-  scanf ("%f", &a);
-
-  printf("Lietotājs,lūdzu, ievadi beigas robežu: ");
-  scanf ("%f", &b);
-
-  printf("Lietotājs,lūdzu, ievadi precizitāti: ");
-  scanf ("%e", &delta_x);
-
-  int2 = (b-a)*(f(a)+f(b))/2.;
-
- while(fabs(int2 - int1)>delta_x){
-  n*=2;
-  h=(b-a)/n;
-  int1=int2;
-  int2=0.;
- for(k=0;k<n;k++) int2+=h*f(a+(k+0.5)*h);
- }
- printf("\nintegral, using rectangle rule: %.5f\n",int2);
-
- int1 =0;
- int2 = (b-a)*(f(a)+f(b))/2.;
- while(fabs(int2 - int1)>delta_x){
-  m*=2;
-  n=2*m;
-  h=(b-a)/n;
-  int1=int2;
-  int2=0.;
- for(k=1;k<=m-1;k++) int2+=2.*(2*f(a+(2*k-1)*h)+f(a+2*k*h));
- int2+=f(a)+f(b)+4*f(b-h);
- int2*=h/3;
- }
- printf("integral, using Simpson\'s rule: %.5f\n",int2);
+	int k,n=2,m=1;
+	float a=0.,b=0,delta_x=0; //ievaditie lielumi
+	float int1=0.,int2=0.,int3=0.,h;
 
 
-}
+	printf("lūdzu, ievadi sākuma robežu: ");
+	scanf("%f", &a);
 
-float f(float x){
- float y1;
- y1 = (1+x)*exp(x);
- return y1;
+	printf("lūdzu, ievadi beigas robežu: ");
+	scanf("%f", &b);
+
+	printf("lūdzu, ievadi precizitāti: ");
+	scanf ("%e", &delta_x);
+
+	//int2 = (b-a)*(f(a)+f(b))/2.; //pamaina funkciju
+	int2=(b-a)*(sin(sqrt(a))*sin(sqrt(a))+sin(sqrt(b))*sin(sqrt(b)))/2.;
+
+	while(fabs(int2 - int1)>delta_x)
+	{
+		n*=2;
+		h=(b-a)/n;
+		int1=int2;
+		int2=0.;
+
+		for(k=0;k<n;k++)
+		{
+			int2+=h*sin(sqrt(a+(k+0.5)*h))*sin(sqrt(a+(k+0.5)*h));
+		}
+	}
+
+	printf("\nintegral, using rectangle rule: %.5f\n",int2);
+
+	int1 =0;
+	//int2 = (b-a)*(f(a)+f(b))/2.;
+	int2=(b-a)*(sin(sqrt(a))*sin(sqrt(a))+sin(sqrt(b))*sin(sqrt(b)))/2.;
+
+	while(fabs(int2 - int1)>delta_x)
+	{
+		m*=2;
+		n=2*m;
+		h=(b-a)/n;
+		int1=int2;
+		int2=0.;
+
+		for(k=1;k<=m-1;k++)
+		{
+			int2+=2.*(2*sin(sqrt(a+(2*k-1)*h))+sin(sqrt(a+2*k*h)));
+			int2+=sin(sqrt(a))*sin(sqrt(a))+sin(sqrt(b))*sin(sqrt(b))+4*sin(sqrt(b-h))*sin(sqrt(b-h));
+			int2*=h/3;
+		}
+	}
+
+	printf("integral, using Simpson\'s rule: %.5f\n",int2);
+
 }
