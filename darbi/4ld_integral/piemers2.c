6b062288343ff8e2abbb0b2094487f341314184b
root <floby132@gmail.com>
2020-01-20 14:54:23 +0000

    20200120_16-54

--- darbi/4ld_integral/piemers2.c
+++ darbi/4ld_integral/piemers2.c
@@ -3,72 +3,81 @@
 
 void main(){
 
-	int k,n=2,m=1;
-	float a=0.,b=0,delta_x=0; //ievaditie lielumi
-	float int1=0.,int2=0.,int3=0.,h;
+	int n=2,m=1;
+
+	float a=0.,b=0,dx; //ievaditie lielumi
+
+	float intgr1=0.,intgr2=0.,intgr3=0.,h; //h-augst
 
 
-	printf("lūdzu, ievadi sākuma robežu: ");
+	printf("Ludzu, ievadi sakuma robezu x0: ");
 	scanf("%f", &a);
 
-	printf("lūdzu, ievadi beigas robežu: ");
+	printf("Ludzu, ievadi beigu robezu x1: ");
 	scanf("%f", &b);
 
-	printf("lūdzu, ievadi precizitāti: ");
-	scanf ("%e", &delta_x);
+	printf("Ludzu, ievadi precizitati delta_x(dx): ");
+	scanf ("%e", &dx);
 
-	int2=(b-a)*(sin(sqrt(a))*sin(sqrt(a))+sin(sqrt(b))*sin(sqrt(b)))/2.;
+	intgr2=(b-a)*(sin(sqrt(a))*sin(sqrt(a))+sin(sqrt(b))*sin(sqrt(b)))/2.;
 
-	while(fabs(int2 - int1)>delta_x)
+	while(fabs(intgr2-intgr1)>dx)
 	{
-		n*=2;
+		n=2*n;
 		h=(b-a)/n;
-		int1=int2;
-		int2=0.;
+		intgr1=intgr2;
+		intgr2=0.;
 
-		for(k=0;k<n;k++)
+		for(int i=0;i<n;i++)
 		{
-			int2+=h*sin(sqrt(a+(k+0.5)*h))*sin(sqrt(a+(k+0.5)*h));
+			intgr2=intgr2+h*sin(sqrt(a+(i+0.5)*h))*sin(sqrt(a+(i+0.5)*h));
 		}
 	}
 
-	printf("\nintegral, using rectangle rule: %.5f\n",int2);
+	printf("\nIntegrala vertiba izmantojot taisnstura metodi: %.5f \n\n",intgr2);
 
- n =2;
- int1 =0;
- int2 = (b-a)*(sin(sqrt(a))*sin(sqrt(a))+sin(sqrt(b))*sin(sqrt(b)))/2.;
- while(fabs(int2 - int1)>delta_x){
-  n*=2;
-  h=(b-a)/n;
-  int1=int2;
-  int2=0.;
- for(k=1;k<n;k++) int2+=sin(sqrt(a+(k)*h));
- int2+=(sin(sqrt(b))*sin(sqrt(b))+sin(sqrt(a))*sin(sqrt(a)))/2;
- int2*=h;
- }
- printf("integrālis, aprēķināts ar trapeču metodi: %.5f\n",int2);
+	n=2;
+	intgr1=0;
+	intgr2=(b-a)*(sin(sqrt(a))*sin(sqrt(a))+sin(sqrt(b))*sin(sqrt(b)))/2.;
 
-        int1 =0;
-        int2=(b-a)*(sin(sqrt(a))*sin(sqrt(a))+sin(sqrt(b))*sin(sqrt(b)))/2.;
+	while(fabs(intgr2-intgr1)>dx)
+	{
+		n*=2;
+		h=(b-a)/n;
+		intgr1=intgr2;
+		intgr2=0.;
 
-        while(fabs(int2 - int1)>delta_x)
+		for(int i=1;i<n;i++)
+		{
+			intgr2+=sin(sqrt(a+(i)*h));
+		}
+
+		intgr2=intgr2+(sin(sqrt(b))*sin(sqrt(b))+sin(sqrt(a))*sin(sqrt(a)))/2;
+		intgr2=intgr2*h;
+	}
+
+	printf("Integrala vertiba izmantojot trapeces metodi: %.5f \n\n",intgr2);
+
+        intgr1=0;
+        intgr2=(b-a)*(sin(sqrt(a))*sin(sqrt(a))+sin(sqrt(b))*sin(sqrt(b)))/2.;
+
+        while(fabs(intgr2-intgr1)>dx)
         {
-                m*=2;
+                m=2*m;
                 n=2*m;
                 h=(b-a)/n;
-                int1=int2;
-                int2=0.;
+                intgr1=intgr2;
+                intgr2=0.;
 
-                for(k=1;k<=m-1;k++)
+                for(int i=1;i<=m-1;i++)
                 {
-                        int2+=2.*(2*sin(sqrt(a+(2*k-1)*h))+sin(sqrt(a+2*k*h)));
-                        int2+=sin(sqrt(a))*sin(sqrt(a))+sin(sqrt(b))*sin(sqrt(b))+4*sin(sqrt(b-h))*sin(sqrt(b-h));
-                        int2*=h/3;
+                        intgr2=intgr2+2.*(2*sin(sqrt(a+(2*i-1)*h))+sin(sqrt(a+2*i*h)));
+                        intgr2=intgr2+sin(sqrt(a))*sin(sqrt(a))+sin(sqrt(b))*sin(sqrt(b))+4*sin(sqrt(b-h))*sin(sqrt(b-h));
+                        intgr2=intgr2*h/3;
                 }
         }
 
-        printf("integral, using Simpson\'s rule: %.5f\n",int2);
-
+        printf("Integrala vertiba izmantojot Simpsona likumu: %.5f \n\n",intgr2);
 
 }
 
