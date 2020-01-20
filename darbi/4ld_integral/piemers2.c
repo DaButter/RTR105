bd83df4089a33cd25a5b42b5fac4e3e795719f7d
root <floby132@gmail.com>
2020-01-20 15:31:55 +0000

    20202001_17-31

--- darbi/4ld_integral/piemers2.c
+++ darbi/4ld_integral/piemers2.c
@@ -3,7 +3,7 @@
 
 void main(){
 
-	int n=2,m=1;
+	int j=2,k=1;
 
 	float a=0.,b=0,dx; //ievaditie lielumi
 
@@ -23,12 +23,12 @@ void main(){
 
 	while(fabs(intgr2-intgr1)>dx)
 	{
-		n=2*n;
-		h=(b-a)/n;
+		j=2*j;
+		h=(b-a)/j;
 		intgr1=intgr2;
 		intgr2=0.;
 
-		for(int i=0;i<n;i++)
+		for(int i=0;i<j;i++)
 		{
 			intgr2=intgr2+h*sin(sqrt(a+(i+0.5)*h))*sin(sqrt(a+(i+0.5)*h));
 		}
@@ -36,18 +36,18 @@ void main(){
 
 	printf("\nIntegrala vertiba izmantojot taisnstura metodi: %.5f \n\n",intgr2);
 
-	n=2;
+	j=2;
 	intgr1=0;
 	intgr2=(b-a)*(sin(sqrt(a))*sin(sqrt(a))+sin(sqrt(b))*sin(sqrt(b)))/2.;
 
 	while(fabs(intgr2-intgr1)>dx)
 	{
-		n*=2;
-		h=(b-a)/n;
+		j=j*2;
+		h=(b-a)/j;
 		intgr1=intgr2;
 		intgr2=0.;
 
-		for(int i=1;i<n;i++)
+		for(int i=1;i<j;i++)
 		{
 			intgr2+=sin(sqrt(a+(i)*h));
 		}
@@ -63,13 +63,13 @@ void main(){
 
         while(fabs(intgr2-intgr1)>dx)
         {
-                m=2*m;
-                n=2*m;
-                h=(b-a)/n;
+                k=2*k;
+                j=2*k;
+                h=(b-a)/j;
                 intgr1=intgr2;
                 intgr2=0.;
 
-                for(int i=1;i<=m-1;i++)
+                for(int i=1;i<=k-1;i++)
                 {
                         intgr2=intgr2+2.*(2*sin(sqrt(a+(2*i-1)*h))+sin(sqrt(a+2*i*h)));
                         intgr2=intgr2+sin(sqrt(a))*sin(sqrt(a))+sin(sqrt(b))*sin(sqrt(b))+4*sin(sqrt(b-h))*sin(sqrt(b-h));
