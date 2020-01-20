4b68d3f470e4b75f3d7c39237097276fee2eb308
root <floby132@gmail.com>
2020-01-20 11:13:21 +0000

    20200120-13-13

--- darbi/3ld_derivatives/recovery.c
+++ darbi/3ld_derivatives/recovery.c
@@ -2,13 +2,10 @@
 #include <math.h>
 
 void saving(int k,float dx,float b, float a);
-
-double function(double x, double x1,double dx);
-
 void main(){
 
-  int k=0;
-  float a=0.,b=0.,x,dx=1.e-1;
+  int k;
+  float a,b,x,dx;
 
   printf("Ludzu, ievadiet sakuma robezu x0: ");
   scanf ("%f", &a);
@@ -20,15 +17,14 @@ void main(){
   scanf ("%e", &dx);
 
   x=a;
-
+  k=0;
   while(x<=b)
   {
 	  k++;
-	  x+= dx;
+	  x=x+dx;
   }
 
  saving(k,dx,a,b);
-
 }
 
 double f(double x, double x1,double dx){
@@ -46,7 +42,7 @@ void saving(int k,float dx,float a, float b){
 	float x[k],y1[k],y2[k],y3[k],y4[k],y5[k];
 
 	FILE *fp = fopen("./atvasinajums.dat", "w");
-	fprintf(fp,"\tx\t\t f(x)\t\tf\'(x)\t\tf\"(x)\t\tf\'(x)\t\tf\"(x)\n");
+	fprintf(fp,"\tx\t\t f(x)\t\tf\'(x)\t\tf\"(x)\n");
 
 
 	for(i=0;i<k;i++)
@@ -62,7 +58,7 @@ void saving(int k,float dx,float a, float b){
 		y3[i] = (2*sqrt(x[i])*sin(sqrt(2*x[i]))-sin(sqrt(2*x[i])))/(4*x[i]*sqrt(x[i])); //2k atvasinajums
 	}
 
-	for(i=0;i<(k-1);i++)
+/*	for(i=0;i<(k-1);i++)
 	{
 		y4[i]= f(y1[i],y1[i+1],dx);
 	}
@@ -72,9 +68,9 @@ void saving(int k,float dx,float a, float b){
 		y5[i] = f(y2[i],y2[i+1],dx);
 	}
 
-	for(i=0;i<k;i++)
+	*/for(i=0;i<k;i++)
 	{
-		fprintf(fp,"%10.2f\t%13.2f\t%13.2f\t%13.2f\t%13.2f\t%13.2f\n",x[i],y1[i],y2[i],y3[i],y4[i],y5[i]);
+		fprintf(fp,"%10.2f\t%13.2f\t%13.2f\t%13.2f\n",x[i],y1[i],y2[i],y3[i]);
 	}
 
  fclose(fp);
