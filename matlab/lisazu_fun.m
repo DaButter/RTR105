2df0550a3dd3d7f461dc7e721affdfe9d22f0d0c
DaButter <54942939+DaButter@users.noreply.github.com>
2020-01-27 09:56:26 +0200

    Create lisazu_fun.m

---
+++ matlab/lisazu_fun.m
@@ -0,0 +1,13 @@
+function lisazu_fun(f1,f2)
+% Skripts kas uzzime Lisažu figūras
+%Call:
+%lisazu_fun(f1,f2)
+
+% 27.01.2020
+% autors Austris
+t =0:0.01:1;
+%f1=9; f2=12;
+x = cos(2*pi*f1*t);
+y = sin(2*pi*f2*t);
+plot(x,y)
+shg
