21216020e2a693aa14301f7014866fcb40898a52
DaButter <54942939+DaButter@users.noreply.github.com>
2020-02-10 10:06:52 +0200

    Add files via upload

---
+++ matlab/lab1/lab_report_example.m
@@ -0,0 +1,21 @@
+%% 1.laboratorijas darbs
+%% Mērījumu datu apstrāde
+%% Mērķi:
+% * Iemācīties apstrādāt mērījumu datus
+% * Iemācīties lietot polyfit,polyval funkcijas
+% * Iemācīties veidot matlab atskaites izmantojot "publish"
+%% Darba programma:
+
+t= 0:0.01:1;
+y=sin(2*pi*t);
+plot(t,y)
+
+%%
+%
+% <<../grafiks22.png>>
+%
+%%
+
+%% Secinājumi
+% Secinājumus raksta par to, ko esi iemācījies šajā darbā
+% Vai arī skatamies darba mērķi, un tad rakstam secinājumus
